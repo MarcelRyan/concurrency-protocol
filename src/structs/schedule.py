359144e79818d2be7ef99f22139b338d0c29f2d4
Edia Zaki <13521141@mahasiswa.itb.ac.id>
2023-11-26 09:50:18 +0700

    feat: schedule

--- src/structs/schedule.py
+++ src/structs/schedule.py
@@ -1,32 +1,37 @@
 from typing import List
 from structs.transaction import Action, Transaction
-from core.lock import LockManager
 
 class Schedule:
     def __init__(self, actions: List[Action]) -> None:
-        self._actions = actions
+        self.actions = actions
+        transaction_dict = {}
+        for action in actions:
+            if action.transaction_id not in transaction_dict.keys():
+                transaction_dict[action.transaction_id] = []
+            transaction_dict[action.transaction_id].append(action)
+        
+        transactions = []
+        for key in transaction_dict.keys():
+            transactions.append(Transaction(key, key, transaction_dict[key]))      
+        self._transactions = transactions
     
     @property
     def actions(self) -> List[Action]:
         return self._actions
-
-    @property
-    def transactions(self) -> List[Transaction]:
-        return self._transactions
-
+    
     @actions.setter
     def actions(self, value: List[Action]) -> None:
         self._actions = value
 
-    @actions.setter
+    @property
+    def transactions(self) -> List[Transaction]:
+        return self._transactions
+    
+    @transactions.setter
     def transactions(self, value: List[Transaction]) -> None:
         self._transactions = value
 
-    def isTransactionExist(self, id: int) -> bool:
-        for transaction in self._transactions:
-            if transaction.id == id:
-                return True
-        return False
+
     
     
     
