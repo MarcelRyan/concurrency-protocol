aee6506dfb367d675af5a12d0d010b97e70caee6
JerichoFletcher <jericho.russel17@gmail.com>
2023-11-30 15:01:02 +0700

    feat: main program

--- src/main.py
+++ src/main.py
@@ -1,10 +1,20 @@
 from argparse import ArgumentParser
 from structs.schedule import Schedule
 from core.cc.tpl import TwoPhaseLockingCC
+from core.cc.occ import OptimisticCC
+from core.cc.mvcc import MultiversionTimestampCC
 import utils.parser as parse
+
+_STRATEGY_SELECT = {
+    'tpl': TwoPhaseLockingCC,
+    'occ': OptimisticCC,
+    'mvcc': MultiversionTimestampCC
+}
+_STRATEGY_DEFAULT = 'tpl'
 
 parser = ArgumentParser(description='Memprotokolkan sebuah konkurensi')
 parser.add_argument('schedule')
+parser.add_argument('--strategy', '-s', choices=list(_STRATEGY_SELECT.keys()), default=_STRATEGY_DEFAULT)
 
 if __name__ == '__main__':
     args = parser.parse_args()
@@ -13,6 +23,6 @@ if __name__ == '__main__':
     sched = Schedule(operations)
     print(f'Input schedule:\n{sched}')
 
-    strategy = TwoPhaseLockingCC()
+    strategy = _STRATEGY_SELECT[args.strategy]()
     sched.apply_cc(strategy)
     print(f'\nNew schedule (using CC strategy "{strategy.__class__.__name__}"):\n{sched}')
